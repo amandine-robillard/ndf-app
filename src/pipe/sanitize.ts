737b85c888f0191e08b42067505b5aeb66889c0d
Amandine Robillard <amandine34.r@gmail.com>
2017-11-07 13:33:44 +0100

    0.1.0: Clean des accents

--- src/pipe/sanitize.ts
+++ src/pipe/sanitize.ts
@@ -7,6 +7,20 @@ export class SanitizeString implements PipeTransform {
 	transform(value, args) {
 		let newValue = value.replace(/ /g, '-');
 		newValue = newValue.toLowerCase();
+		newValue = this.cleanAccent(newValue);
 		return newValue;
+	}
+
+	/**
+	 * Supprime les accents
+	 * @param  {string} str
+	 * @return {string}
+	 */
+	cleanAccent(str) {
+		str = str.replace(/[ÀÁÂÃÄÅ]/g,"a");
+		str = str.replace(/[àáâãäå]/g,"a");
+		str = str.replace(/[ÈÉÊË]/g,"e");
+		str = str.replace(/[éèêë]/g,"e");
+		return str;
 	}
 }
